ab37204a6c5be7d690b421827628efe512f3cbd9
Nicola Gulmini <nicolagulmini@gmail.com>
2021-08-17 15:53:24 +0200

    Update temp.py

--- temp.py
+++ temp.py
@@ -3,13 +3,4 @@ from Bio import SeqIO
 adhesin_dataset = list(SeqIO.parse("adh.fasta", "fasta"))
 not_adhesin_dataset = list(SeqIO.parse("not_adh.fasta", "fasta"))
 
-print(len(adhesin_dataset))
-print(len(not_adhesin_dataset))
-
-lista_aminoacidi = []
-for aa in adhesin_dataset[0]:
-    if aa not in lista_aminoacidi:
-        lista_aminoacidi.append(aa)
-print(len(lista_aminoacidi))
-
-print(adhesin_dataset[0].seq)
+test_protein = adhesin_dataset[0].seq
