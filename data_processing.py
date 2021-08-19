430a61a98515111e2ee5eeaee38b50842e0f9776
Nicola Gulmini <62892813+nicolagulmini@users.noreply.github.com>
2021-08-19 21:22:21 +0200

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -236,6 +236,34 @@ def process(positive_ds, negative_ds):
             x.append(tmp)
             y.append(0)
     return x, y
+
+def process_one_dataset(ds):
+    # data processing
+    x = []
+
+    for protein in ds:
+        tmp = [
+                    aminoacids_frequencies(protein.seq),
+                    multiplet_frequencies(protein.seq, 3),
+                    multiplet_frequencies(protein.seq, 4),
+                    multiplet_frequencies(protein.seq, 5),
+                    dipeptide_frequencies(protein.seq),
+                    charge_composition(protein.seq),
+                    hydrophobic_composition(protein.seq)
+        ]
+        cond = 1
+        for el in tmp:
+            if isinstance(el, int):
+                cond = 0
+        if cond == 1:
+            for el in tmp:
+                for entry in el:
+                    if (entry == float("inf") or entry == float("-inf")):
+                        cond = 0
+                        break
+        if cond == 1:
+            x.append(tmp)
+    return x
     
 def split_ds(x, y, shuffler):
     x = x[shuffler]
