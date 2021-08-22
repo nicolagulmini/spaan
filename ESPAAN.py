dd9f33613cbdb684f3c48a8d3c42204bdb30c2f6
Nicola Gulmini <62892813+nicolagulmini@users.noreply.github.com>
2021-08-22 16:55:08 +0200

    Delete ESPAAN.py

--- ESPAAN.py
+++
@@ -1,6 +0,0 @@
-from Bio import SeqIO
-
-adhesin_dataset = list(SeqIO.parse("adh.fasta", "fasta"))
-not_adhesin_dataset = list(SeqIO.parse("not_adh.fasta", "fasta"))
-
-test_protein = adhesin_dataset[0].seq
