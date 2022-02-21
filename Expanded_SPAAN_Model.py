068a702697b3013fbd7efc14d460a20b0129ad92
Nicola Gulmini <62892813+nicolagulmini@users.noreply.github.com>
2022-02-21 14:17:08 +0000

    Update Expanded_SPAAN_Model.py

--- Expanded_SPAAN_Model.py
+++ Expanded_SPAAN_Model.py
@@ -1,6 +1,7 @@
 from tensorflow.keras.layers import Input
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Concatenate
+from tensorflow.keras.layers import Dropout
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
 
@@ -12,7 +13,8 @@ class Expanded_SPAAN_Model:
         aa_freq = Input(shape=(20))
         dense_1 = Dense(30, activation='sigmoid')(aa_freq)
         inter_dense = Dense(20, activation='sigmoid')(dense_1)
-        dense_1_2 = Dense(1, activation='sigmoid')(inter_dense)
+        dropout_1 = Dropout(.2)(inter_dense)
+        dense_1_2 = Dense(1, activation='sigmoid')(dropout_1)
         
         # model 2.1
         multiplet_freq_1 = Input(shape=(20))
@@ -32,30 +34,35 @@ class Expanded_SPAAN_Model:
         # concatenate outputs
         concat_1 = Concatenate()([dense_2_1_2, dense_2_2_2, dense_2_3_2])
         inter_dense_concat = Dense(20)(concat_1)
-        dense_2_4 = Dense(1, activation='sigmoid')(inter_dense_concat)
+        dropout_2 = Dropout(.2)(inter_dense_concat)
+        dense_2_4 = Dense(1, activation='sigmoid')(dropout_2)
         
         # model 3
         dipept_freq = Input(shape=(400))
         dense_3 = Dense(500, activation='sigmoid')(dipept_freq)
         inter_dense_5 = Dense(100, activation='sigmoid')(dense_3)
-        dense_3_2 = Dense(1, activation='sigmoid')(inter_dense_5)
+        dropout_3 = Dropout(.2)(inter_dense_5)
+        dense_3_2 = Dense(1, activation='sigmoid')(dropout_3)
         
         # model 4
         charge_comp = Input(shape=(15))
         dense_4 = Dense(30, activation='sigmoid')(charge_comp)
         inter_dense_6 = Dense(20, activation='sigmoid')(dense_4)
-        dense_4_2 = Dense(1, activation='sigmoid')(inter_dense_6)
+        dropout_4 = Dropout(.2)(inter_dense_6)
+        dense_4_2 = Dense(1, activation='sigmoid')(dropout_4)
         
         # model 5
         hydrophob_comp = Input(shape=(50))
         dense_5 = Dense(40, activation='sigmoid')(hydrophob_comp)
         inter_dense_7 = Dense(20, activation='sigmoid')(dense_5)
-        dense_5_2 = Dense(1, activation='sigmoid')(inter_dense_7)
+        dropout_5 = Dropout(.2)(inter_dense_7)
+        dense_5_2 = Dense(1, activation='sigmoid')(dropout_5)
         
         # final concatenation
         concat_2 = Concatenate()([dense_1_2, dense_2_4, dense_3_2, dense_4_2, dense_5_2])
         final_inter_dense = Dense(20)(concat_2)
-        final_dense = Dense(1, activation='sigmoid')(final_inter_dense)
+        dropout_final = Dropout(.1)(final_inter_dense)
+        final_dense = Dense(1, activation='sigmoid')(dropout_final)
         
         model = Model(inputs=[aa_freq, multiplet_freq_1, multiplet_freq_2, multiplet_freq_3, dipept_freq, charge_comp, hydrophob_comp], outputs=final_dense)
         model.compile(optimizer=Adam(learning_rate=0.001), loss='binary_crossentropy', metrics='accuracy')
