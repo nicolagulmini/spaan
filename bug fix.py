a029f58242b91fae0522917cc7f18324e9aca0be
Nicola Gulmini <nicolagulmini@gmail.com>
2022-04-28 14:43:06 +0100

    Update bug fix.py

--- bug fix.py
+++ bug fix.py
@@ -8,3 +8,4 @@ import tensorflow
 # load the datasets (it requires about 1 minute)
 x, y = process(list(SeqIO.parse("./data/original_adh_dataset.fasta", "fasta")), list(SeqIO.parse("./data/original_negative_dataset.fasta", "fasta")))
 x_train, y_train, x_val, y_val, x_test, y_test = split_ds(np.array(x), np.array(y), np.random.permutation(len(x)))
+
